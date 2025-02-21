a025fe90dd241e3197675078795c92f010e34306
vb2007 <admin@vb2007.hu>
2025-02-22 00:08:37 +0100

    added paste rate limit, removed not working js

--- html/page/_script/pastebin.php
+++ html/page/_script/pastebin.php
@@ -15,11 +15,28 @@ $mysqli->query("CREATE TABLE IF NOT EXISTS `pastebin`(
 if(isset($_POST['paste']) && isset($_POST['pasteTitle'])) {
     include "auth.php";
 
-    $pasteTitle = $_POST["pasteTitle"];
-    $paste = $_POST["paste"];
-
     //jelenleg bejelentkezett felhasználó neve
     $addedBy = $_SESSION['username'];
+
+    //5 sec rate limit
+    $query = $mysqli->prepare("SELECT dateAdded FROM pastebin WHERE addedBy = ? ORDER BY dateAdded DESC LIMIT 1");
+    $query->bind_param("s", $addedBy);
+    $query->execute();
+    $result = $query->get_result();
+    
+    if($lastUrl = $result->fetch_assoc()) {
+        $lastUrlTime = strtotime($lastUrl['dateAdded']);
+        $currentTime = time();
+        
+        if(($currentTime - $lastUrlTime) < 5) {
+            echo "Please wait 5 seconds between submitting pastes.";
+            $mysqli->close();
+            exit;
+        }
+    }
+
+    $pasteTitle = $_POST["pasteTitle"];
+    $paste = $_POST["paste"];
 
     //beteszi a linket a táblába
     $query = $mysqli->prepare("INSERT INTO pastebin (pasteTitle, paste, addedBy, dateAdded) VALUES (?, ?, ?, NOW())");
--- html/page/pastebin.php
+++ html/page/pastebin.php
@@ -55,7 +55,7 @@
     <?php include '_common/footer.php'; ?>
     <!--Script import-->
     <script src="https://cdn.vb2007.hu/webstatic/js/pastebin_handler.js"></script>
-    <script src="https://cdn.vb2007.hu/webstatic/js/pastebin_dynamic_line_displayer.js"></script>
+    <!-- <script src="https://cdn.vb2007.hu/webstatic/js/pastebin_dynamic_line_displayer.js"></script> -->
     <script src="https://cdn.vb2007.hu/webstatic/js/rainbow_shit.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
