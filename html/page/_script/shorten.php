26c96ca202251ad87f6e591ba60366cfd7a9edf9
vb2007 <admin@vb2007.hu>
2025-02-24 00:08:24 +0100

    made urls non nullable

--- html/page/_script/shorten.php
+++ html/page/_script/shorten.php
@@ -4,8 +4,8 @@ include_once("_config.php");
 //létrehozza a táblát (ha nem létezik)
 $mysqli->query("CREATE TABLE IF NOT EXISTS `urlShortener`(
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    `url` text DEFAULT NULL,
-    `shortUrl` text DEFAULT NULL,
+    `url` text NOT NULL,
+    `shortUrl` text NOT NULL,
     `addedBy` text DEFAULT NULL,
     `dateAdded` datetime DEFAULT NULL,
     `votes` int(11) NOT NULL DEFAULT 0,
